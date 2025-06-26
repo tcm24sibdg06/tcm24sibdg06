619405885b3c77177ddad73df1c8949d7aa23a3a
RSPedra <a042419@umaia.pt>
2025-06-26 16:44:56 +0100

    Delete src/api-clinica/src directory

--- src/api-clinica/src/models/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './consulta.model';
-export * from './especialidade.model';
-export * from './funcionario.model';
-export * from './historico-consulta.model';
-export * from './medico.model';
-export * from './paciente.model';
-export * from './registo-clinico.model';
-
