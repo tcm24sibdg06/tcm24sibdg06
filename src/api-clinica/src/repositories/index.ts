3359631f2aa99b9be85cafa339236ef38cff6dde
Bernardo-Azevedo-045200 <a045200@umaia.pt>
2025-07-09 17:19:07 +0100

    Delete src directory

--- src/api-clinica/src/repositories/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './consulta.repository';
-export * from './especialidade.repository';
-export * from './funcionario.repository';
-export * from './historico-consulta.repository';
-export * from './medico.repository';
-export * from './paciente.repository';
-export * from './registo-clinico.repository';
